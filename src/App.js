2591b17f89e79c40c67af567937289cee09c079a
manu <manu.villar.vitoria@gmail.com>
2020-05-28 19:58:47 +0200

    conditional load of components  when verify itself if there are results

--- src/App.js
+++ src/App.js
@@ -2,42 +2,64 @@ import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 import Clima from './components/Clima';
+import Error from './components/Error';
 
 function App() {
 
-//State del formulario
-const [busqueda, guardarBusqueda] = useState({
-    ciudad:'',
-    pais:''
-});
+  //State del formulario
+  const [busqueda, guardarBusqueda] = useState({
+      ciudad:'',
+      pais:''
+  });
 
-const [consultar, guardarConsultar] = useState(false);
-
-const [resultado, guardarResultado] = useState({});
-
-//extraer ciudad y país con destructuring
-const {ciudad, pais} = busqueda;
-
-//Este Hook detecta los cambios que pases en el array de dependencias.
-useEffect(() => {
-
-  if(consultar) { // Colocamos el condicional para que no nos salga primero el error 404.
-
-    const consultarAPI = async () => {
-
-      const appID = 'e548f2f4039680a13ac57b2df3e5cb53';
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appID}`;
+  const [consultar, guardarConsultar] = useState(false);
+  const [resultado, guardarResultado] = useState({});
+  const [error, guardarError]= useState(false);
   
-      const response = await fetch(url);
-      const res = await response.json();
-  
-      guardarResultado(res);
-      guardarConsultar(false); //Para hacer múltiples consultas sin necesidad de recargar página.
+  //extraer ciudad y país con destructuring
+  const {ciudad, pais} = busqueda;
+
+  //Este Hook detecta los cambios que pases en el array de dependencias.
+  useEffect(() => {
+
+    if(consultar) { // Colocamos el condicional para que no nos salga primero el error 404.
+
+      const consultarAPI = async () => {
+
+        const appID = 'e548f2f4039680a13ac57b2df3e5cb53';
+        //Añadimos una 's' al protocolo 'http' para que nos deje hacer el deployment.
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appID}`;
+    
+        const response = await fetch(url);
+        const res = await response.json();
+    
+        guardarResultado(res);
+        guardarConsultar(false); //Para hacer múltiples consultas sin necesidad de recargar página.
+
+        //detecta si hubo resultados correctos en la consulta
+        if(resultado.cod === "404") {
+          guardarError(true);
+        } else {
+          guardarError(false);
+        }
+      
+      }
+      
+      consultarAPI();
     }
-    consultarAPI();
-  }
+    //Para que no nos aparezca un warning que indique que nos falta las dependencias relacionadas con pais y ciudad
+    //colocamos el siguiente comentario inferior para desactivar ese warning.
+    
+    // eslint-disable-next-line 
+    },[consultar]);
 
-},[consultar]);
+  //Carga condicional de componentes
+  let componente;
+  if(error) {
+    componente = <Error mensaje="No hay resultados" />
+  } else  {
+    componente = <Clima resultado={resultado} />
+  }
 
 
   return (
@@ -56,10 +78,7 @@ useEffect(() => {
                 />
               </div>
               <div className="col m6 s12">
-                <Clima
-                  resultado={resultado} 
-
-                />
+                {componente}
               </div>
             </div>
           </div>
